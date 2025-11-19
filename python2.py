017fea1358c56aaa2ac8fab2ae970d77d8905ae9
Rayan Mohammadinia <rmohammadinia@browning.edu>
2025-11-19 14:19:14 +0000

    created numbers function

---
+++ python2.py
@@ -0,0 +1,9 @@
+n = int(input("Enter Gregorian Year -> ").strip())
+
+if n % 4 != 0:
+    print("True")
+elif n % 4 == 0 and n % 100 == 0:
+    print("False")
+elif n % 4 == 0 and n % 100 == 0 and n % 400 == 0:
+    print("True")
+    
